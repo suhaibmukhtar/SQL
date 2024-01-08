3207cba4d6aff8cb2a841e7fd93727285ea9bc5b
Suhaib Mukhtar <suhaibmukhtar2@gmail.com>
2024-01-09 02:43:44 +0530

    ## Third file
    contains all the code related to how to load data from a sql file

---
+++ Load_data_from_source_file.sql
@@ -0,0 +1,90 @@
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mydb               |
+| mysql              |
+| performance_schema |
+| school             |
+| suhaib             |
+| sys                |
++--------------------+
+7 rows in set (0.01 sec)
+
+mysql> source C:\Users\suhaib mukhtar\Downloads\mysqlsampledatabase\mysqlsampledatabase.sql
+Query OK, 1 row affected (0.01 sec)
+
+Database changed
+Query OK, 0 rows affected, 1 warning (0.01 sec)
+
+Query OK, 0 rows affected, 1 warning (0.00 sec)
+
+Query OK, 0 rows affected, 1 warning (0.00 sec)
+
+Query OK, 0 rows affected, 1 warning (0.00 sec)
+
+Query OK, 0 rows affected, 1 warning (0.00 sec)
+
+Query OK, 0 rows affected, 1 warning (0.01 sec)
+
+Query OK, 0 rows affected, 1 warning (0.00 sec)
+
+Query OK, 0 rows affected, 1 warning (0.00 sec)
+
+Query OK, 0 rows affected (0.04 sec)
+
+Query OK, 0 rows affected, 1 warning (0.07 sec)
+
+Query OK, 0 rows affected (0.04 sec)
+
+Query OK, 0 rows affected (0.11 sec)
+
+Query OK, 0 rows affected (0.11 sec)
+
+Query OK, 0 rows affected (0.05 sec)
+
+Query OK, 0 rows affected (0.06 sec)
+
+Query OK, 0 rows affected, 1 warning (0.09 sec)
+
+Query OK, 7 rows affected (0.01 sec)
+Records: 7  Duplicates: 0  Warnings: 0
+
+Query OK, 110 rows affected (0.03 sec)
+Records: 110  Duplicates: 0  Warnings: 0
+
+Query OK, 7 rows affected (0.01 sec)
+Records: 7  Duplicates: 0  Warnings: 0
+
+Query OK, 23 rows affected (0.01 sec)
+Records: 23  Duplicates: 0  Warnings: 0
+
+Query OK, 122 rows affected (0.03 sec)
+Records: 122  Duplicates: 0  Warnings: 0
+
+Query OK, 326 rows affected (0.04 sec)
+Records: 326  Duplicates: 0  Warnings: 0
+
+Query OK, 273 rows affected (0.02 sec)
+Records: 273  Duplicates: 0  Warnings: 0
+
+Query OK, 2996 rows affected (0.20 sec)
+Records: 2996  Duplicates: 0  Warnings: 0
+
+mysql> show databases
+    -> ^C
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| classicmodels      |
+| information_schema |
+| mydb               |
+| mysql              |
+| performance_schema |
+| school             |
+| suhaib             |
+| sys                |
++--------------------+
+8 rows in set (0.00 sec)
